8b599197dcfa69b50283f730bda47e6aae56647d
marcelobentes <marcelo.maia.m@gmail.com>
2022-03-03 22:04:56 -0300

    Criando um classe de Exceção
    Criando um classe de Exceção

---
+++ src/java_excecao/ExcecaoProcessarNota.java
@@ -0,0 +1,10 @@
+package java_excecao;
+
+@SuppressWarnings("serial")
+public class ExcecaoProcessarNota extends Exception{
+	
+	public ExcecaoProcessarNota(String erro) {
+		super(erro);
+	}
+
+}
--- src/main/TestInterface.java
+++ src/main/TestInterface.java
@@ -1,13 +1,19 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 import classesAuxiliares.FuncaoAutenticar;
 import interfaces.PermitirAcesso;
+import java_excecao.ExcecaoProcessarNota;
 
 public class TestInterface {
 
 	/* Usando interfaces melhorando.*/
+	@SuppressWarnings({ "null", "resource", "unused" })
 	public static void main(String[] args) {
 		
 		try {
@@ -15,9 +21,18 @@ public class TestInterface {
 		String login = JOptionPane.showInputDialog("Informe o login:");
 		String senha = JOptionPane.showInputDialog("Informe a senha:");
 		
-		FuncaoAutenticar funcaoautenticar = null;
+		FuncaoAutenticar funcaoautenticar = new FuncaoAutenticar ();
 		
 		PermitirAcesso permitiAcesso = new Diretor (login, senha);
+		
+		try {
+			
+			File arquivo = new File ("c:\file.txt");
+			
+			Scanner entrada = new Scanner(arquivo);
+		} catch (FileNotFoundException e) {
+			throw new ExcecaoProcessarNota("Erro a processar arquivo não encontrado.");
+		}
 		
 		if(funcaoautenticar.autenticar(permitiAcesso)) {
 			JOptionPane.showInternalMessageDialog(null,"Acesso permitido...");
