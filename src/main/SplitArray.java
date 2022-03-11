3c348f06fabc912a61b227c13f21d532891bd221
marcelobentes <74937617+marcelobentes@users.noreply.github.com>
2022-03-10 23:45:49 -0300

    Convertendo um texto split
    em Array
    Array para Lista e Lista para Array

---
+++ src/main/SplitArray.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SplitArray {
+
+	public static void main(String[] args) {
+		//convertendo um texto Split em array
+		
+		String texto = "Marcelo, Curso Java, 70, 86, 75";
+		String [] valoresArray = texto.split(",");
+		
+		System.out.println("Nome: "+ valoresArray[0]);
+		System.out.println("Curso: "+ valoresArray[1]);
+		System.out.println("Nota 1: "+ valoresArray[2]);
+		System.out.println("Nota 2: "+ valoresArray[3]);
+		System.out.println("Nota 3: "+ valoresArray[4]);
+
+		
+		//Convertendo array em um lista
+		
+		List<String> list = Arrays.asList(valoresArray);
+		System.out.println("======Converção para Lista======");
+		for (String valorstring : list) {
+			
+			System.out.println(valorstring);
+		}
+		
+		//Convertendo lista para Array
+		
+		String [] conversaoArray = list.toArray(new String [5]);
+		System.out.println("======Converção para Array======");
+		
+		for (int i = 0; i < conversaoArray.length; i++) {
+			
+			System.out.println(conversaoArray[i]);
+			
+		}
+	}
+
+}
