ccd69c577d0f5036ca44402393a2668e597275a2
marcelobentes <74937617+marcelobentes@users.noreply.github.com>
2022-03-14 21:18:10 -0300

    Executando Thread
    Executando Thread em backlog

--- src/thread/TestThread.java
+++ src/thread/TestThread.java
@@ -6,17 +6,32 @@ public class TestThread {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		for (int i = 0; i < 10; i++) {
-			//executando a thread com sleep, para dar uma parada na execução
+		//Criando uma thread em backlog
+		new Thread() {
+			public void run() { //executa o codigo da rotina
+				
+				for (int i = 0; i < 10; i++) {
+					//executando a thread com sleep, para dar uma parada na execução
+					
+					System.out.println("Executando rotina de enviar e-mail");
+					
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						
+						e.printStackTrace();
+					}
+				}
+			};
 			
-			System.out.println("Executando rotina de enviar e-mail");
-			
-			Thread.sleep(1000);
-		}
+		}.start(); //liga a thread que fica processando paralelamente por trás
 		
-		System.out.println("Finalizado o codigo de teste da Thread");
+		
+		
 		
 		JOptionPane.showMessageDialog(null, "Sistama continua executando para o usuario.");
+	
+		System.out.println("Finalizado o codigo de teste da Thread");
 	}
 
 }
