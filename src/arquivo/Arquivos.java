4d35a4a5ce8e77978dc1d1c6069fe555bce34d1e
marcelobentes <74937617+marcelobentes@users.noreply.github.com>
2022-03-28 22:01:05 -0300

    Escrevendo uma lista no arquivo
    Escrevendo uma lista no arquivo

--- src/arquivo/Arquivos.java
+++ src/arquivo/Arquivos.java
@@ -3,10 +3,27 @@ package arquivo;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arquivos {
 
 	public static void main(String[] args) throws IOException {
+		
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.setNome("Marcelo");
+		pessoa1.setEmail("marcelo@.br");
+		pessoa1.setIdade(25);
+		
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.setNome("Mabel");
+		pessoa2.setEmail("marbel@.br");
+		pessoa2.setIdade(8);
+		
+		Pessoa pessoa3 = new Pessoa();
+		pessoa3.setNome("Samuel");
+		pessoa3.setEmail("samuel@.br");
+		pessoa3.setIdade(6);
 		
 		File arquivo1 = new File("/home/marcelo/DESENVOLVIMENTO/WORKSPACE JAVAWEB/JavaWeb2/src/arquivo/arquivo1.txt");
 		
@@ -15,10 +32,20 @@ public class Arquivos {
 			arquivo1.createNewFile();
 		}
 		
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
+		pessoas.add(pessoa1);
+		pessoas.add(pessoa2);
+		pessoas.add(pessoa3);
+		
 		FileWriter escreverNoArquivo = new FileWriter(arquivo1);//criando o objeto para escrever
 		
-		escreverNoArquivo.write("Meu primeiro texto.");//escrevendo no arquivo
-		escreverNoArquivo.write("\nMeu segundo texto.");//escrevendo no arquivo
+		for (Pessoa p : pessoas) {
+			
+			escreverNoArquivo.write(p.getNome() +";"+ p.getEmail() +";"+p.getIdade()+ "\n");
+		}
+		
+				
 		escreverNoArquivo.flush();//persisti alteração
 		escreverNoArquivo.close();//fechar o arquivo
 	}
