bc43cef44b4da8c698f86dfcc6f882b19a71187e
marcelobentes <74937617+marcelobentes@users.noreply.github.com>
2022-03-31 23:13:33 -0300

    ApachePoi4 e ApachePoiEditando
    ApachePoi4 e ApachePoiEditando

---
+++ src/arquivo/ApachePoi4.java
@@ -0,0 +1,70 @@
+package arquivo;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ApachePoi4 {
+
+	public static void main(String[] args) throws IOException {
+		
+		//Lendo um arquivo xls
+		FileInputStream entrada = new FileInputStream("D:\\DESENVOLVIMENTO\\WORKSPACE\\JavaWeb-FullStack\\src\\arquivo\\arquivo_excel.xls");
+		
+		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(entrada);//prepara o arquivo do excel para leitura
+		HSSFSheet planilha = hssfWorkbook.getSheetAt(0);//pega a primeira planilha
+		
+		Iterator<Row> linhaIterator = planilha.iterator();
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
+		while (linhaIterator.hasNext()) {//enquanto estiver linha no excel
+			Row linha = linhaIterator.next();
+			
+			Iterator<Cell> celulas = linha.iterator();
+			
+			Pessoa pessoa = new Pessoa();
+			
+			while(celulas.hasNext()) {
+				
+				Cell cell = celulas.next();
+				
+				switch (cell.getColumnIndex()) {
+				
+				case 0:
+					pessoa.setNome(cell.getStringCellValue());
+					break;
+					
+				case 1:
+					pessoa.setEmail(cell.getStringCellValue());
+					break;
+				case 2:
+					pessoa.setIdade(Double.valueOf(cell.getNumericCellValue()).intValue());
+					break;
+					
+				}
+				
+				
+			}//fim das celulas da linha
+			
+			pessoas.add(pessoa);
+			
+			
+		}
+		
+		entrada.close();//termina de ler o arquivo
+		
+		for (Pessoa p : pessoas) {
+			System.out.println(p);
+		}
+	
+	
+	}
+
+}
---
+++ src/arquivo/ApachePoiEditando.java
@@ -0,0 +1,53 @@
+
+package arquivo;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+
+public class ApachePoiEditando {
+
+	public static void main(String[] args) throws IOException {
+	
+		File file = new File ("D:\\DESENVOLVIMENTO\\WORKSPACE\\JavaWeb-FullStack\\src\\arquivo\\arquivo_excel.xsl");
+		
+		FileInputStream entrada = new FileInputStream (file);
+		
+		HSSFWorkbook hssfworkbook = new HSSFWorkbook(entrada);//preparando a entrada do arquivo
+		
+		HSSFSheet planilha = hssfworkbook.getSheetAt(0);//pegando a planilha
+		
+		Iterator <Row> linhaIterator = planilha.iterator();
+		
+		while (linhaIterator.hasNext()) {//enquato tiver linha
+			Row linha = linhaIterator.next();//Dados da pessoa da linha
+			
+			int numeroCelulas = linha.getPhysicalNumberOfCells();//quantidade de linha
+
+			Cell cell = linha.createCell(numeroCelulas);//criar nova celula na linha
+			cell.setCellValue("5.486,25");
+						
+		}
+		
+		entrada.close();
+		
+		
+		FileOutputStream saida = new FileOutputStream(file);
+		hssfworkbook.write(saida);
+		saida.flush();
+		saida.close();
+		
+		System.out.println("Planilha Atualizada");
+		
+		
+	}
+
+}
